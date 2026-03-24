14b620f58d31182b9cd33fa94ccc3b17e45924f1
Varsha Rani <varsharani.r.07@gmail.com>
2026-03-24 22:15:29 +0530

    UC2 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,15 @@
 public class HelloApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
 
+        // Check if a command-line argument is provided
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            // Default message if no argument is given
+            System.out.println("Hello, World!");
+        }
+
+    }
 }
